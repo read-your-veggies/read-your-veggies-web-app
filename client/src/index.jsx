b5ad995f5c459e63e63a53df8eb80917b2ed104d
Scott McCreary <scott.mccreary.11@gmail.com>
2018-06-19 19:54:29 -0400

    Add to boilerplate

---
+++ client/src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    } 
+  }
+
+  render () {
+    return (
+      <div className="app-container">
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
