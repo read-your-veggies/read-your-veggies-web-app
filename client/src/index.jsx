82a770d9c73f1a589aee4c53feeaa50df7470614
Philip Gonzalez <pgonzalez115@gmail.com>
2018-06-21 19:39:04 -0400

    Partial implementation of initial state

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,17 +4,21 @@ import App from './components/App.jsx';
 
 import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
+import ApolloClient from 'apollo-boost';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-//this is our graphQl endpoint
-const httpLink = new HttpLink({ uri: 'https://localhost:5000/graphql' });
-
+var defaults;
+var resolvers;
+var typeDefs;
 
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
+  uri: 'https://localhost:5000/graphql',
+  clientState: {
+    defaults,
+    resolvers,
+    typeDefs
+  }
 });
 
 
