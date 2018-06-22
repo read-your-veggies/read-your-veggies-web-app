36cbc49387802b5aa7c96b33f52cc4a821482e0b
Ian Schmidt <IanS73@gmail.com>
2018-06-22 15:22:14 -0400

    began a facebook auth file

---
+++ server/facebookAuth.js
@@ -0,0 +1,20 @@
+const passport = require('passport');
+const fbStrategy = require('passport-facebook');
+require('dotenv').config();
+
+passport.serializeUser( (user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser( (user, done) => {
+    done(null, user);
+});
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.APP_ID,   // These are not yet defined in .env file
+    clientSecret: process.env.APP_SECRET,
+    callbackURL: process.env.CALLBACK_URL},
+    function(accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+));
