603d4ff35f40a2f3c5a14c3056bbdf266e124e1b
Philip Gonzalez <pgonzalez115@gmail.com>
2018-06-25 14:01:17 -0400

    Css changes to article cards and speedometer

--- client/src/components/HealthSpeedometer.jsx
+++ client/src/components/HealthSpeedometer.jsx
@@ -8,19 +8,20 @@ class HealthSpeedometer extends React.Component {
 
   render() {
     return (
-      <div className='speedometer-wrapper' style={{width: '100%', height: "100%", background: "#EFEFEF"}}>
+      <div className='speedometer-wrapper' style={{width: '100%', height: "100%"}}>
         <ReactSpeedometer
           minValue={-100}
           maxValue={100}
           
           // this value will ultimately be a query for the user's score!
-          value={0}
+          value={Math.floor(Math.random() * 201) - 100}
+          currentValueText='Media Health: ${value}'
 
           needleColor="rgb(25,120,29)"
           startColor="blue"
           segments={7}
           endColor="red"
-          textColor="rgb(25,120,29)"
+          textColor='#ff77e8'
           needleTransitionDuration={10000}
           needleTransition="easeElastic"
           width={this.props.width}
