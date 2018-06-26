9e6e6cb1098b87909f418889ff5ede45dab0dd01
Philip Gonzalez <pgonzalez115@gmail.com>
2018-06-26 10:27:41 -0400

    Implement ArticleCarousel component

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,45 +1,15 @@
 import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
 import { Query } from "react-apollo";
-import { GET_ARTICLES_FROM_SERVER, GET_ONE_FULL_ARTICLE } from '../apollo/serverQueries';
 import { GET_TEAM_NAME_FROM_LOCAL_STATE } from '../apollo/localQueries';
-import ArticleCard from './ArticleCard.jsx';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
+import ArticleCarousel from './ArticleCarousel.jsx';
 
 
 class Dashboard extends Component {
   render() {
     return(
       <div> 
-        {/* <h1>Doctor's Orders</h1>   */}
-
-        <Query query={GET_ARTICLES_FROM_SERVER}>
-          {({ loading, error, data }) => {
-            if (loading) return "Loading...";
-            if (error) return `Error! ${error.message}`;
-
-            return (
-              <CarouselProvider
-                naturalSlideWidth={500}
-                naturalSlideHeight={600}
-                totalSlides={20}
-                visibleSlides={4}
-              >        
-                <Slider>
-                  {data.articles.map((article, i) => (
-                      <Slide index={i}>
-                        <ArticleCard article={article}/>
-                      </Slide>
-                  ))}
-                </Slider>
-                <ButtonBack>Back</ButtonBack>
-                <ButtonNext>Next</ButtonNext>
-              </CarouselProvider>
-            );
-          }}
-        </Query>
-
+        <ArticleCarousel />
         <Query query={GET_TEAM_NAME_FROM_LOCAL_STATE}>
           {({ data, client }) => {
             return (
