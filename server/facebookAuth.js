bac39d9a74ce577c3bb91df098cbecb21b5f6c65
Ian Schmidt <IanS73@gmail.com>
2018-06-28 15:08:16 -0400

    done for now.  we may tie this func to a worker

--- server/facebookAuth.js
+++ server/facebookAuth.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook');
 const {User} = require('../db/schemas.js')
+const getPolRatio = require('./getRegionalPolitics.js');
 
 require('dotenv').config();
 
@@ -19,10 +20,7 @@ passport.use(new FacebookStrategy({
     profileFields: ['email', 'birthday', 'location', 'displayName', 'hometown', 'age_range']
   },
   function(accessToken, refreshToken, profile, done) {
-    // Add to database here
-    // console.log('User', User);
-    process.nextTick( () => {    // This is a pretty new function for me.
-      console.log('profile', profile);
+    process.nextTick( () => {
       User.findOne({facebookId: profile.id}, (err, user) => {
         if (err) return done(err);
 
@@ -35,22 +33,21 @@ passport.use(new FacebookStrategy({
           newUser.emails = profile._json.email;
           newUser.facebookUrl = profile.profileUrl;
           newUser.location = profile._json.location.name;
+          newUser.locPolRatio = 0; // We will run the getPolRatio function as a daily worker to fill this field in properly
           newUser.hometown = profile._json.hometown.name;
+          newUser.homePolRatio = 0; // Will run getPolRatio func as a daily worker for this.
           newUser.age_range = JSON.stringify(profile._json.age_range);
 
           console.log('new user to be saved', newUser);
-
           newUser.save( (err) => {
             if (err) {
               console.log(err);
             }
-
             return done(null, newUser);
           })
         }
       })
     })
-    // done(null, profile);
   }
 ));
 
--- server/getRegionalPolitics.js
+++ server/getRegionalPolitics.js
@@ -4,33 +4,32 @@ require('dotenv').config();
 
 // Write a function here that we will export to facebookAuth
 const getRatio = function (city) {
+  // Idea: We can pass in the user ID as a 2nd argument and invoke the User model 
+  // to add the information to the db from within this function itself.
+  // We would have to import User schema and perform a findOneAndUpdate
   let county = countyConvert[city];
   let key = process.env.ELECTIONS_BY_COUNTY_API_KEY;
-
-  axios.get(`https://electionsbycounty.com/api/v1.0/${key}/elections/${county}/Presidential`)
-      .then( (response) => {
-
-        // Filter out just the elections since 2000
-        let filteredElections = response.data.filter( election => {
-          return election.Date[0] === '2';
-        });
-
-        // Get the republican and democrat votes and make a ratio for each election.
-        let ratios = filteredElections.map( (election) => {
-          return election.DemocraticVotes / election.RepublicanVotes;
-        });
-
-        // Take the mean average of all the ratios.
-        let avgRatio = ratios.reduce( (total, currVal) => {
-          return total + currVal;
-        }) / ratios.length;
-        
-        // Now with the average ratio we... add to user's profile in mongo?
-
-      })
-      .catch( (err) => {
-        console.log('error!', err);
-      })
+  
+  return axios.get(`https://electionsbycounty.com/api/v1.0/${key}/elections/${county}/Presidential`)
+    .then( (response) => {
+      // Filter out just the elections since 2000
+      let filteredElections = response.data.filter( election => {
+        return election.Date[0] === '2';
+      });
+      
+      // Get the republican and democrat votes and make a ratio for each election.
+      let ratios = filteredElections.map( (election) => {
+        return election.DemocraticVotes / election.RepublicanVotes;
+      });
+      
+      // Take the mean average of all the ratios, and return.
+      return ratios.reduce( (total, currVal) => {
+        return total + currVal;
+      }) / ratios.length;
+    })
+    .catch( (err) => {
+      console.log('error!', err);
+    })
 }
 
-// getRatio('Cascade Valley, Washington');
+module.exports = getRatio
