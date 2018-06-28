8e384effe43a3de9ddba90c0f2b0dd1a500cff79
Ian Schmidt <IanS73@gmail.com>
2018-06-28 12:13:22 -0400

    progress on election ratios

--- server/getRegionalPolitics.js
+++ server/getRegionalPolitics.js
@@ -1,3 +1,36 @@
 const countyConvert = require('../db/data/citiesCounties.js');
+const axios = require('axios');
+require('dotenv').config();
 
-console.log(countyConvert["Norwood, New Jersey"]);
+// Write a function here that we will export to facebookAuth
+const getRatio = function (city) {
+  let county = countyConvert[city];
+  let key = process.env.ELECTIONS_BY_COUNTY_API_KEY;
+
+  axios.get(`https://electionsbycounty.com/api/v1.0/${key}/elections/${county}/Presidential`)
+      .then( (response) => {
+
+        // Filter out just the elections since 2000
+        let filteredElections = response.data.filter( election => {
+          return election.Date[0] === '2';
+        });
+
+        // Get the republican and democrat votes and make a ratio for each election.
+        let ratios = filteredElections.map( (election) => {
+          return election.DemocraticVotes / election.RepublicanVotes;
+        });
+
+        // Take the mean average of all the ratios.
+        let avgRatio = ratios.reduce( (total, currVal) => {
+          return total + currVal;
+        }) / ratios.length;
+        
+        // Now with the average ratio we... add to user's profile in mongo?
+
+      })
+      .catch( (err) => {
+        console.log('error!', err);
+      })
+}
+
+// getRatio('Cascade Valley, Washington');
