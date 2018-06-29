30db141b9756f66d4809293c912896544d366301
Scott McCreary <scott.mccreary.11@gmail.com>
2018-06-29 14:50:31 -0400

    health styling

--- client/src/components/CompletedArticles.jsx
+++ client/src/components/CompletedArticles.jsx
@@ -5,7 +5,6 @@ import { GET_USER_FROM_DB, GET_ONE_FULL_ARTICLE } from '../apollo/serverQueries.
 import {GET_USER_INFO} from '../apollo/localQueries.js';
 import Panel from 'react-bootstrap/lib/Panel';
 
-
 class CompletedArticles extends React.Component {
   constructor(props) {
     super(props);
@@ -22,33 +21,28 @@ class CompletedArticles extends React.Component {
                   if (loading) return "Loading...";
                   if (error) return `Error! ${error.message}`;
                   var completedArticleInfo = JSON.parse(data.user.completed_articles);
-                  console.log(completedArticleInfo);
                   var completedArticleKeys = Object.keys(completedArticleInfo);
                   return (
                     <div className="completed-articles-container">{completedArticleKeys.map((articleId) => {
                       return (
-                        <p></p>
-                        // <Query
-                        //   query={GET_ONE_FULL_ARTICLE}
-                        //   variables={{ _id: articleId.articleId }}
-                        // >
-                        //   {({ loading, error, data }) => {
-                        //     if (loading) return "Loading...";
-                        //     if (error) return `Error! ${error.message}`;
-                        //     return (
-                        //       <Panel className="completed-article">
-                        //         <Panel.Heading>
-                        //         </Panel.Heading>
-                        //         <Panel.Body>
-                        //           <p>Article Stance:{completedArticleInfo[articleId].articleStance}</p>
-                        //           <p>Date Completed: {completedArticleInfo[articleId].completed}</p>
-                        //           <p>Nutritional Value: {completedArticleInfo[articleId].nutritionalValue}</p>
-                        //           <p>Your Stance At Time: {completedArticleInfo[articleId].onboardStance}</p>
-                        //         </Panel.Body>
-                        //       </Panel>
-                        //     );
-                        //   }}
-                         //</Query>        
+                        <Query query={GET_ONE_FULL_ARTICLE} variables={{_id: articleId}}>
+                          {({ loading, error, data }) => {
+                            if (loading) return "Loading...";
+                            if (error) return `Error! ${error.message}`;
+                            var article = data.article;
+                            return (
+                              <Panel className="completed-article">
+                                <Panel.Heading className="completed-article-title">
+                                  <a href={article.url}>{article.title}</a>
+                                </Panel.Heading>
+                                <Panel.Body>
+                                  <p>Nutrition for you: {completedArticleInfo[articleId].nutritionalValue}</p>
+                                  <img src={article.image} />                             
+                                </Panel.Body>
+                              </Panel>
+                            )
+                          }}
+                        </Query>                       
                       );
                     })}
                     </div>
