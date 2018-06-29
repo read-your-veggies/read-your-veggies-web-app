c31f81619074e296ab443c537d4f242561013d3c
Ian Schmidt <IanS73@gmail.com>
2018-06-29 19:24:49 -0400

    removed unused imports from article modal

--- client/src/components/ArticleModal.jsx
+++ client/src/components/ArticleModal.jsx
@@ -6,10 +6,6 @@ import Button from 'react-bootstrap/lib/Button';
 import Modal from 'react-bootstrap/lib/Modal';
 import Voter from './Voter.jsx';
 import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
-import Badge from 'react-bootstrap/lib/Badge';
-import { DELETE_ARTICLE } from '../apollo/resolvers';
-import { GET_ARTICLES_FROM_SERVER, GET_ONE_FULL_ARTICLE } from '../apollo/serverQueries';
-import { Query, ApolloConsumer } from "react-apollo";
 
 class ArticleModal extends React.Component {
   constructor(props) {
