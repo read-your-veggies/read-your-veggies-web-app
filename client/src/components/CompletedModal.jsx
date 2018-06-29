3bd4576f96e23d365b5ed7032cda0290f949bf14
Ian Schmidt <IanS73@gmail.com>
2018-06-29 19:27:18 -0400

    removed unused imports from completedModal.jsx

--- client/src/components/CompletedModal.jsx
+++ client/src/components/CompletedModal.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import Panel from 'react-bootstrap/lib/Panel';
-import FormGroup from 'react-bootstrap/lib/FormGroup';
-import Radio from 'react-bootstrap/lib/Radio';
 import Button from 'react-bootstrap/lib/Button';
-import Checkbox from 'react-bootstrap/lib/Checkbox';
-import ControlLabel from 'react-bootstrap/lib/ControlLabel';
-import { UPDATE_ARTICLE_VOTES } from '../apollo/resolvers';
-import { Mutation } from "react-apollo";
 import Modal from 'react-bootstrap/lib/Modal';
 import { withRouter } from "react-router-dom";
 
