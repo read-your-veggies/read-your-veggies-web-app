d2b4bbce6dccae90d32ac981ae50bdafe35022d7
Ian Schmidt <IanS73@gmail.com>
2018-06-29 19:30:19 -0400

    removed unused imports from voter.jsx

--- client/src/components/Voter.jsx
+++ client/src/components/Voter.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import Panel from 'react-bootstrap/lib/Panel';
 import FormGroup from 'react-bootstrap/lib/FormGroup';
-import Radio from 'react-bootstrap/lib/Radio';
 import Button from 'react-bootstrap/lib/Button';
 import Checkbox from 'react-bootstrap/lib/Checkbox';
-import ControlLabel from 'react-bootstrap/lib/ControlLabel';
 import { UPDATE_ARTICLE_VOTES, UPDATE_USER_VOTES } from '../apollo/resolvers';
 import { Mutation } from "react-apollo";
 import Modal from 'react-bootstrap/lib/Modal';
