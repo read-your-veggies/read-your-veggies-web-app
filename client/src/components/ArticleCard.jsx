e42eb37f5cc42d69445a24ff3d77fc58d8a4103b
Ian Schmidt <IanS73@gmail.com>
2018-06-29 19:24:24 -0400

    removing unused imports from article card

--- client/src/components/ArticleCard.jsx
+++ client/src/components/ArticleCard.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import Popover from 'react-bootstrap/lib/Popover';
 import Panel from 'react-bootstrap/lib/Panel';
-import Tooltip from 'react-bootstrap/lib/Tooltip';
 import Button from 'react-bootstrap/lib/Button';
-import Modal from 'react-bootstrap/lib/Modal';
 import Voter from './Voter.jsx';
-import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
 import Badge from 'react-bootstrap/lib/Badge';
 
 import ArticleModal from './ArticleModal.jsx';
