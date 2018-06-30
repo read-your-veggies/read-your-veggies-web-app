e2a53f1e5c17dc8a3ca37a0c3444533f5897d052
Philip Gonzalez <pgonzalez115@gmail.com>
2018-06-30 17:49:11 -0400

    Add mlab connections

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
+
 var mongoose = require('mongoose');
 var databasePath = process.env.DATABASE_PATH || 'mongodb://localhost/readyourveggies';
-
 mongoose.connect(databasePath);
 var db = mongoose.connection;
 
@@ -11,5 +11,45 @@ const connectToDb = (callback) => {
     callback();
   });
 }
+
+var usersMongoose = require('mongoose');
+var usersDatabasePath = process.env.USERS_DATABASE_PATH;
+usersMongoose.connect(usersDatabasePath);
+var usersDb = usersMongoose.connection;
+
+const connectToUsersDb = (callback) => {
+  usersDb.on('error', console.error.bind(console, 'connection error:'));
+  usersDb.once('open', function() {
+    console.log('Connected to USERS db.');
+    callback();
+  });
+}
+
+var articlesMongoose = require('mongoose');
+var articlesDatabasePath = process.env.ARTICLES_DATABASE_PATH;
+articlesMongoose.connect(articlesDatabasePath);
+var articlesDb = articlesMongoose.connection;
+
+const connectToArticlesDb = (callback) => {
+  articlesDb.on('error', console.error.bind(console, 'connection error:'));
+  articlesDb.once('open', function() {
+    console.log('Connected to ARTICLES db.');
+    callback();
+  });
+}
+
+var sourcesMongoose = require('mongoose');
+var sourcesDatabasePath = process.env.SOURCES_DATABASE_PATH;
+sourcesMongoose.connect(sourcesDatabasePath);
+var sourcesDb = sourcesMongoose.connection;
+
+const connectToSourcesDb = (callback) => {
+  sourcesDb.on('error', console.error.bind(console, 'connection error:'));
+  sourcesDb.once('open', function() {
+    console.log('Connected to SOURCES db.');
+    callback();
+  });
+}
+
 
 module.exports = { connectToDb, db };
