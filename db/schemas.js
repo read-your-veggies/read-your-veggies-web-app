6f50ea519a4e871c3eb7f43aad1289725380cb0e
Scott McCreary <scott.mccreary.11@gmail.com>
2018-07-05 16:57:29 -0400

    remove redundancy

--- db/schemas.js
+++ db/schemas.js
@@ -1,41 +1,41 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const db = require('./index.js').db;
-const articleDbConn = require('./index.js').articleDbConn;
+//const db = require('./index.js').db;
+//const articleDbConn = require('./index.js').articleDbConn;
 const userDbConn = require('./index.js').userDbConn;
-const sourceDbConn = require('./index.js').sourceDbConn;
+//const sourceDbConn = require('./index.js').sourceDbConn;
 
-const articleSchema = new Schema({
-  url: {type: String, unique: true},
-  title: String,
-  author: Array, // news api always returns an array of strings here
-  source: String,
-  description: String,
-  fullText: String,
-  articleStance: Number,
-  image: String,
-  votes: {
-    agree: {
-      summedUserStance: Number,
-      totalVotes: Number,
-    },
-    disagree: {
-      summedUserStance: Number,
-      totalVotes: Number,
-    },
-    fun: {
-      summedUserStance: Number,
-      totalVotes: Number,
-    },
-    bummer: {
-      summedUserStance: Number,
-      totalVotes: Number,
-    },
-  },
-});
+// const articleSchema = new Schema({
+//   url: {type: String, unique: true},
+//   title: String,
+//   author: Array, // news api always returns an array of strings here
+//   source: String,
+//   description: String,
+//   fullText: String,
+//   articleStance: Number,
+//   image: String,
+//   votes: {
+//     agree: {
+//       summedUserStance: Number,
+//       totalVotes: Number,
+//     },
+//     disagree: {
+//       summedUserStance: Number,
+//       totalVotes: Number,
+//     },
+//     fun: {
+//       summedUserStance: Number,
+//       totalVotes: Number,
+//     },
+//     bummer: {
+//       summedUserStance: Number,
+//       totalVotes: Number,
+//     },
+//   },
+// });
 
-const Article = articleDbConn.model('Article', articleSchema);
+// const Article = articleDbConn.model('Article', articleSchema);
 
 const userSchema = new Schema({
   authenticationInfo: String,
@@ -64,15 +64,15 @@ const userSchema = new Schema({
 
 const User = userDbConn.model('User', userSchema);
 
-const sourceSchema = new Schema({
-  name: String,
-  articles: [String],
-  titles: [String],
-  fullTexts: [String],
-  titlesPersonality: String,
-  fullTextsPersonality: String,
-});
+// const sourceSchema = new Schema({
+//   name: String,
+//   articles: [String],
+//   titles: [String],
+//   fullTexts: [String],
+//   titlesPersonality: String,
+//   fullTextsPersonality: String,
+// });
 
-const Source = sourceDbConn.model('Source', sourceSchema);
+// const Source = sourceDbConn.model('Source', sourceSchema);
 
-module.exports = { Article, User, Source }
+module.exports = { User }
