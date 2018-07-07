0c826f66e5f42f81563f674d0027cfaea31b82f8
Scott McCreary <scott.mccreary.11@gmail.com>
2018-07-06 20:01:58 -0400

    style modal

--- client/src/components/ArticleModal.jsx
+++ client/src/components/ArticleModal.jsx
@@ -19,25 +19,32 @@ class ArticleModal extends React.Component {
 
   render() {
     return (
-      <Modal show={this.props.show} onHide={() => this.props.handleClose('article')}>
+      <Modal className="article-modal" show={this.props.show} onHide={() => this.props.handleClose('article')}>
         <Modal.Header closeButton>
           <Modal.Title>{this.props.article.title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           {/* <h4>{this.props.article.description}</h4> */}
-          <div className="modal-body-left">
+          {/* <div className="modal-body-left">
             <img className="article-image" src={this.props.article.image} />
           </div>
-          <div className="modal-body-right">
-            <Panel>
-              <Panel.Body className="article-full-text">{this.props.article.fullText}</Panel.Body>
-              <Button onClick={(e) => {
-                e.preventDefault();
-                this.completeArticle();
-              }}
-              >Complete article!</Button>
+          <div className="modal-body-right"> */}
+            <Panel className="article-panel">
+              <img className="article-image" src={this.props.article.image} />
+              <Panel.Body className="article-full-text">
+                {this.props.article.fullText}
+                <div className="complete-article-wrapper">
+                  <Button bsSize="large" bsStyle="success" onClick={(e) => {
+                      e.preventDefault();
+                      this.completeArticle();
+                    }}
+                    >
+                  Complete Article
+                  </Button>
+                </div>
+              </Panel.Body>
             </Panel>
-          </div>
+            
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={
