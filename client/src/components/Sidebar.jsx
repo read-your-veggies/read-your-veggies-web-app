e94cc7a81a605c410935b40655911229c62c8f52
Scott McCreary <scott.mccreary.11@gmail.com>
2018-07-11 17:31:21 -0400

    convert to functional components

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -16,8 +16,7 @@ class Sidebar extends Component {
   };
 
   componentDidMount() {
-    //subtract 147 px (size of header);
-    this.setState({divHeight: window.innerHeight - 147 + 'px'});
+    this.setState({divHeight: window.innerHeight - 100 + 'px'});
   }
 
   goTo = (displayName, link) => {
@@ -27,12 +26,12 @@ class Sidebar extends Component {
   }
 
   render() {
-    //const tooltip = <Tooltip id="modal-tooltip">See Your Report</Tooltip>;
+    const tooltip = <Tooltip id="modal-tooltip">See Your Report</Tooltip>;
     const userId = this.props.getUserInfo.data.userInfo.userId;
     const displayName = this.props.getUserInfo.data.userInfo.displayName;
 
     return (
-      <div id='dash-sidebar' >
+      <div id='dash-sidebar' style={{height: this.state.divHeight}}>
         <Query query={GET_USER_INFO}>
           {(({data}) => {
             return (
@@ -42,14 +41,14 @@ class Sidebar extends Component {
                   if (error) return `Error! ${error.message}`;
                   const userStance = JSON.parse(data.user.user_stance);
                   return (
-                    <div id='dash-sidebar-inner'>
+                    <div id='dash-sidebar-inner' style={{height: this.state.divHeight}}>
                       <div onClick={() => this.goTo(displayName, "dashboard")} className='sidebar-container'>
                         <h3>Home</h3>
                         <img src="./assets/logo.png" />
                       </div>
                       <UserInfo location={this.props.location} displayName={displayName} userId={userId} goTo={this.goTo}/>
-                      <div onClick = {() => this.goTo(displayName, "report")} className='sidebar-container speedometer-container'>{' '}
-                        {/* <OverlayTrigger overlay={tooltip}> */}
+                      <OverlayTrigger overlay={tooltip}>
+                        <div onClick = {() => this.goTo(displayName, "report")} className='sidebar-container speedometer-container'>{' '}
                           <h3 id="stance-title">Stance</h3>
                           <HealthSpeedometer 
                             height={100}
@@ -61,9 +60,8 @@ class Sidebar extends Component {
                             max={1}
                             sidebar={true}
                           />
-                        {/* </OverlayTrigger> */}
-                        {' '}
-                      </div>
+                        </div>
+                      </OverlayTrigger>
                       <div onClick={() => this.goTo(displayName, "about")} className='sidebar-container'>
                         <h3 id="about-title">About</h3>
                         <img src="./assets/bargraph.png" />
