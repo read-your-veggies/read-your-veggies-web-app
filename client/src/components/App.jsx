ecc922e07cbf12df00968f0dd0be15b9c61176bb
Ian Schmidt <IanS73@gmail.com>
2018-07-11 11:55:43 -0400

    creating fake guest profile

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -17,8 +17,46 @@ class App extends Component {
   }
   
   componentDidMount() {
+    // If guestLoggedIn: Set the auth headers to match the guest profile
+    // OR can we just use the updateUserInfo function below?
+
     axios.get('/checkAuthHeaders').then((res) => {
-      if (res.headers.user !== undefined) {
+      if (this.state.guestLoggedIn) {
+        this.props.history.push('/dashboard');
+        let user = {
+          name: 'Guest',
+          _id: '5b4520fc863031826ary52fgg',
+          health: 0,
+          user_stance: 0.234613425,
+          recently_read: [],
+          locPolRatio: -.334523,
+          homePolRatio: .1243544,
+          onboard_stance: -.23546534645,
+          onboard_information: {
+
+          },
+          reading_stance: [
+
+          ],
+          browsing_history: [],
+          browsing_history_stance: [],
+          facebookId: '432346456',
+          name: "Guesty McGuestFace",
+          emails: 'notAnEmail@whatwho.xyz',
+          location: 'Hicksville, New York',
+          hometown: 'Butte, Montana',
+          
+
+        };
+
+        this.props.updateUserInfo({
+          variables: {
+            theDisplayName: user.name,
+            theProvider: 'GuestLogin',
+            theUserId: user._id,
+          }
+        })
+      } else if (res.headers.user !== undefined) {
         // Redirect user to /dashboard with react router.
         this.props.history.push('/dashboard');
         const user = (JSON.parse(res.headers.user));
@@ -35,6 +73,14 @@ class App extends Component {
       }
     });
   }
+
+  loginGuest = () => {
+    this.setState({
+      guestLoggedIn: true,
+    }, console.log(this.state))
+  }
+
+
 
   render() {
     return (
@@ -68,7 +114,13 @@ class App extends Component {
               </Query>
             }
           />
-          <Route path='/login' component={Login} />
+
+          <Route path={'/login'} 
+            component={() => 
+              <Login loginGuest={this.loginGuest} />
+            } 
+          />
+
           <Route path={"/health"}
             component={() => 
               <Query query={GET_USER_INFO}>
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { withRouter } from "react-router-dom";
 //
-const Login = () => (
+const Login = (props) => (
   <div className="login-page">
     <h2><a href = '/auth/facebook'>Login with Facebook</a></h2>
-    <h2><a href = ''>Login as Guest</a></h2>
+    <h2><a onClick={props.loginGuest}>Login as Guest</a></h2>
   </div>
 )
 
